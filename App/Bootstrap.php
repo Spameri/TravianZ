45f88ab8eafc226254f68052d0230a85c840f351
VBoss <vcevela@gmail.com>
2016-01-24 13:29:43 +0100

    added install.neon

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -1,16 +1,18 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-$configurator = new Nette\Configurator;
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+return call_user_func(function () {
+	require_once __DIR__ . '/../vendor/autoload.php';
+	$configurator = new \Nette\Configurator;
+	$configurator->setTempDirectory(__DIR__ . '/../temp');
+	$configurator->enableDebugger(__DIR__ . '/../log');
+	$configurator->addConfig(__DIR__ . '/config/config.neon');
+	$configurator->addConfig(__DIR__ . '/config/install.neon');
+	if (file_exists(__DIR__ . '/config/config.local.neon')) {
+		$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+	}
+	$configurator->createRobotLoader()
+		->addDirectory(__DIR__)
+		->register();
 
-$configurator->enableDebugger(__DIR__ . '/../log');
-$configurator->setTempDirectory(__DIR__ . '/../temp');
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-//$configurator->addConfig(__DIR__ . '/config/config.local.neon');
-$container = $configurator->createContainer();
-
-return $container;
+	return $configurator->createContainer();
+});
--- App/InstallModule/Presenters/ConfigPresenter.php
+++ App/InstallModule/Presenters/ConfigPresenter.php
@@ -3,6 +3,7 @@
 namespace App\InstallModule\Presenters;
 
 use Nette;
+use App;
 
 class ConfigPresenter extends Nette\Application\UI\Presenter
 {
