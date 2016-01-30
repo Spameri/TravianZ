c873f0a2727925c88c4fc23dec83367955ae6ee9
Spamer <vcevela@gmail.com>
2016-01-30 10:31:08 +0100

    modified links to inner and outter village

--- App/GameModule/Controls/Header/HeaderControl.php
+++ App/GameModule/Controls/Header/HeaderControl.php
@@ -19,21 +19,34 @@ class HeaderControl extends Nette\Application\UI\Control
 	 * @var App\FrontModule\Model\User\UserService
 	 */
 	private $userService;
+	/**
+	 * @var App\FrontModule\Model\VData\VDataModel
+	 */
+	private $VDataModel;
 
 
 	public function __construct(
 		App\GameModule\Model\MData\MDataModel $MDataModel,
 		App\GameModule\Model\NData\NDataModel $NDataModel,
-		App\FrontModule\Model\User\UserService $userService
+		App\FrontModule\Model\User\UserService $userService,
+		App\FrontModule\Model\VData\VDataModel $VDataModel
 	) {
 		$this->MDataModel = $MDataModel;
 		$this->NDataModel = $NDataModel;
 		$this->userService = $userService;
+		$this->VDataModel = $VDataModel;
 	}
 
 
 	public function render()
 	{
+		$id = $this->presenter->getParameter('id');
+		if ( ! $id) {
+			/** @var \stdClass $field */
+			$field = $this->VDataModel->getByUser($this->presenter->user->getId());
+			$id = $field->wref;
+		}
+		$this->template->villageId = $id;
 		$this->template->unread = $this->MDataModel->countUnread($this->presenter->getUser()->getId()) ? TRUE : FALSE;
 		$this->template->report = $this->NDataModel->countUnread($this->presenter->getUser()->getId()) ? TRUE : FALSE;
 		$this->template->plusActive = $this->userService->hasPlus($this->presenter->getUser()->getId());
